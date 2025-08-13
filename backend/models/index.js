27c401a98b1ed0c2909f035dad0156f9cf9522cf
Paras <240441@softwarica.edu.np>
2025-08-13 12:09:21 +0545

    new commit

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,17 +1,4 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+import { sequelize } from "../config/db.js";
 
 import UserModel from "./User.js";
 import FileModel from "./File.js";
